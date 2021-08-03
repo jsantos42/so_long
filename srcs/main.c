5dc575148834a61d997362ea98775ecc4457759a
Joao Santos <jsantos@c2r1s5.42lisboa.com>
2021-08-03 16:18:41 +0100

    I can print a linegit status

--- srcs/main.c
+++ srcs/main.c
@@ -12,13 +12,16 @@ typedef struct s_data {
 
 
 int on_click(int key, void *param);
-char*	my_pixel_put(t_data *data, int x, int y, int color);
+void	my_pixel_put(t_data *data, int x, int y, int color);
+
 
 int main()
 {
 	void    *connection;
 	void    *window;
 	t_data	data;
+	int		x;
+	int 	y;
 
 	connection = mlx_init();
 	if (connection)
@@ -27,7 +30,14 @@ int main()
 //		mlx_pixel_put(connection, window, 250, 250, 25500);
 		data.img = mlx_new_image(connection, 1920, 1080);
 		data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
-		data.addr = my_pixel_put(&data, 50, 50, 0x00FF0000);
+		x = 0;
+		y = 1;
+		while (x < data.line_length) // && y < 10)
+		{
+			my_pixel_put(&data, x, y, 0x00FF0000);
+			x++;
+//			y++;
+		}
 		mlx_put_image_to_window(connection, window, data.img, 0, 0);
 		mlx_key_hook(window, on_click, (void *)0);
 		mlx_loop(connection);
@@ -35,13 +45,21 @@ int main()
 	return 0;
 }
 
-char *my_pixel_put(t_data *data, int x, int y, int color)
+/*
+**	Here dst gets the sum  of the address of the origin (coordinates 0,0, the
+**	leftmost and uppermost point) and the offset of the desired coordinates
+**	(in order to print to pixel (5,5), for example, it needs to go down y times
+**	(and each step it goes down corresponds to a whole line_length) and needs
+**	to go right x times (and each step it goes right corresponds to the number
+**	of bytes used in each pixel).
+*/
+
+void	my_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = data->addr + (y * data->line_length + (x * (data->bits_per_pixel / 8)));
 	*(unsigned int*)dst = color;
-	return(dst);
 }
 
 
