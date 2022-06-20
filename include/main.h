6e810a96a2c3b45b8066da0efcbb0de7f3b01378
Joao Santos <ctw01895@criticaltechworks.com>
2022-06-20 12:49:35 +0100

    Now ready for linux.

--- include/main.h
+++ include/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 # define MAIN_H
 
-# include "../libs/minilibx_mms_20200219/mlx.h"
-# include "../libs/libft/include/libft.h"
+# include "mlx.h"
+# include "../lib/libft/include/libft.h"
 # include <fcntl.h>
 
 /*
